fc98b30f5a80c94a624b7fb60ace8bea44453f2b
Scott Kyle <scott.m.kyle@gmail.com>
2021-06-12 13:36:15 -0400

    added comprehensive docstrings to multibody module

--- skydy/output/get_output_dir.py
+++ skydy/output/get_output_dir.py
@@ -7,6 +7,12 @@ DIR_NAME = "./out"
 def get_output_dir(output_dir=None):
     """Get the output directory, which is compatible
     with current directory.
+
+    Args:
+        output_dir (str or None): the desired output directory
+
+    Returns:
+        output_dir (str): the output directory, based on where function is called from.
     """
     if output_dir is None:
         if not os.path.exists(DIR_NAME):
