e11ca122ada8ded754f22f1bfb504dfb8536f10d
Scott Kyle <scott.m.kyle@gmail.com>
2021-06-12 09:54:12 -0400

    Configuration module documentation.

--- skydy/configuration/Configuration.py
+++ skydy/configuration/Configuration.py
@@ -10,15 +10,20 @@ NUM_COORDS = 6
 
 class Configuration(CoordinateSymbols):
     def __init__(self, name):
-        """A body's configuration is nothing other than a description of its pose (where it is, and how it is oriented).
-        As such, it is decribed by a vector of positions, and a matrix of rotations. All bodies can have up to 6 DOFs
-        , i.e., directions in which it can move. By applying constraints, a body can have as little as zero DOFs.
+        """A body's configuration is nothing other than a description of its pose
+        (where it is, and how it is oriented).
 
-        A Configuration inherits from CoordinateSymbols, as it is solely related to a body's name, and the 6 CoordinateSymbols that
-        describe it.
+        As such, it is decribed by a vector of positions, and a matrix of rotations.
+        All bodies can have up to 6 DOFs, i.e., directions in which it can move.
+
+        By applying constraints, a body can have as little as zero DOFs.
+
+        A Configuration inherits from CoordinateSymbols, as it is solely related to a
+        body's name, and the 6 CoordinateSymbols that describe it.
 
         Args:
-            name (int or str): the name for the symbols. This will form the superscript, i.e., the body the symbols refer to.
+            name (int or str): the name for the symbols. This will form the superscript,
+            i.e., the body the symbols refer to.
 
         Returns:
             None
@@ -58,7 +63,8 @@ class Configuration(CoordinateSymbols):
         return sym.Matrix([sym.diff(var, sym.Symbol("t")) for var in self.velocities()])
 
     def apply_constraint(self, idx, const_value=0):
-        """Apply a coordinate constraint to the free configuration of the body. A constraint is nothing but a coordinate having constant value.
+        """Apply a coordinate constraint to the free configuration of the body.
+        A constraint is nothing but a coordinate having constant value.
 
         This indices for each coordinate are:
             0 -> x
@@ -70,7 +76,8 @@ class Configuration(CoordinateSymbols):
 
         Args:
             idx (int): the index to apply the constriaint to.
-            const_value (int or float): the constant value to substitute in for the coordinate at the index. Default value is zero.
+            const_value (int or float): the constant value to substitute in for the
+            coordinate at the index. Default value is zero.
 
         Returns:
             None
@@ -81,7 +88,7 @@ class Configuration(CoordinateSymbols):
                 >>> from skydy.configuration import Configuration
                 >>> body_name = "1"
                 >>> body_config = Configuration(body_name)
-                >>> # Apply a translational constraint to the z-axis, at a constant height of 5 m.
+                >>> # Apply a translational constraint to the z-axis, at a height of 5 m.
                 >>> body_config.apply_constraint(2, 5)
                 >>> # Apply a rotational constraint about the y-axis
                 >>> body_config.apply_constraint(4, 0)
@@ -96,7 +103,8 @@ class Configuration(CoordinateSymbols):
     def reset_constraints(self):
         """Reset the constraints, i.e., remove any restrictions on movement.
 
-        In short, the position and rotation are the free configuration matrices determined on object instantiation.
+        In short, the position and rotation are the free configuration matrices
+        determined on object instantiation.
         """
         self.assign_values(np.ones(self.values().shape))
         self.pos_body = self.__pos_free.copy()
@@ -110,7 +118,8 @@ class Configuration(CoordinateSymbols):
             symbols (sympy.matrices.dense.MutableDenseMatrix or list): a list of symbols.
 
         Returns:
-            free_symbols (list): a list of free symbols from the input list, based on the free configuration indices.
+            free_symbols (list): a list of free symbols from the input list, based on
+            the free configuration indices.
 
         Example:
             See free_coordinates, free_velocities, free_accelerations below.
